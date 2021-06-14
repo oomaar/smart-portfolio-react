aff501b2f1179758deb5935c30270772588b3076
Omar <omaar_5@hotmail.com>
2021-06-14 23:50:51 +0200

    feat: Skills part-1

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,12 +1,20 @@
+import { Section, SectionSubtitle, SectionTitle } from "../../GlobalStyle";
 import {
     SkillsContainer,
 } from "./styledSkills";
 
 const Skills = () => {
     return (
-        <SkillsContainer>
-            Skills
-        </SkillsContainer>
+        <Section>
+            <SectionTitle>Skills</SectionTitle>
+            <SectionSubtitle>My technical level</SectionSubtitle>
+
+            <SkillsContainer className="container grid">
+                <div>
+                    <Skill />
+                </div>
+            </SkillsContainer>
+        </Section>
     );
 };
 
