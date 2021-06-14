582ce6be87f55e4b46d6c9d8a807adce563ae996
Omar <omaar_5@hotmail.com>
2021-06-14 23:01:34 +0200

    feat: setup About

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { Header, Home } from "./Components";
+import { About, Header, Home } from "./Components";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Home />
+      <About />
     </div>
   );
 };
