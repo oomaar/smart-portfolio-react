f01bc782cd1db5c7bcdd8e21a35db5cf5c92ae9c
Omar <omaar_5@hotmail.com>
2021-06-14 23:56:13 +0200

    feat: setup Skill

---
+++ src/Components/Skill/Skill.js
@@ -0,0 +1,13 @@
+import {
+    SkillsContent,
+} from "./styledSkill";
+
+const Skill = () => {
+    return (
+        <SkillsContent>
+            Single Skill
+        </SkillsContent >
+    );
+};
+
+export default Skill;
--- src/Components/index.js
+++ src/Components/index.js
@@ -2,3 +2,4 @@ export { default as Header } from "./Header/Header";
 export { default as Home } from "./Home/Home";
 export { default as About } from "./About/About";
 export { default as Skills } from "./Skills/Skills";
+export { default as Skill } from "./Skill/Skill";
