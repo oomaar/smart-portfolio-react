626383ba1563eb408f2e77b1ac42356b92f1ce93
Omar <omaar_5@hotmail.com>
2021-06-14 22:19:11 +0200

    feat: Home Part-1

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,97 @@
+import UilLinkedinAlt from "@iconscout/react-unicons/icons/uil-linkedin-alt";
+import UilGithubAlt from "@iconscout/react-unicons/icons/uil-github-alt";
+import UilFacebookF from "@iconscout/react-unicons/icons/uil-facebook-f";
+import UilMessage from "@iconscout/react-unicons/icons/uil-message";
+import UilMouseAlt from "@iconscout/react-unicons/icons/uil-mouse-alt";
+import UilArrowDown from "@iconscout/react-unicons/icons/uil-arrow-down";
+
+import { Button, Section } from "../../GlobalStyle";
+import {
+    HomeContaier,
+    HomeContent,
+    HomeSocial,
+    SocialIcon,
+    HomeImage,
+    HomeBlob,
+    BlobImage,
+    HomeData,
+    HomeTitle,
+    HomeSubtitle,
+    HomeDescription,
+    HomeScroll,
+    ScrollButton,
+    ScrollButtonText,
+} from "./styledHome";
+
 const Home = () => {
     return (
-        <h1>
-            Home
-        </h1>
+        <Section>
+            <HomeContaier className="container grid">
+                <HomeContent className="grid">
+                    <HomeSocial>
+                        <SocialIcon href="https://www.linkedin.com" target="_blank">
+                            <UilLinkedinAlt />
+                        </SocialIcon>
+                        <SocialIcon href="https://www.github.com" target="_blank">
+                            <UilGithubAlt />
+                        </SocialIcon>
+                        <SocialIcon href="https://www.facebook.com" target="_blank">
+                            <UilFacebookF />
+                        </SocialIcon>
+                    </HomeSocial>
+
+                    <HomeImage>
+                        <HomeBlob
+                            viewBox="0 0 200 187"
+                            xmlns="http://www.w3.org/2000/svg"
+                        // xmlns:xlink="http://www.w3.org/1999/xlink"
+                        >
+                            <mask id="mask0" mask-type="alpha">
+                                <path
+                                    d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 
+                    130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 129.362C2.45775 
+                    97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 -0.149132 97.9666 
+                    0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"
+                                />
+                            </mask>
+                            <g mask="url(#mask0)">
+                                <path
+                                    d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 
+                    165.547 130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 
+                    129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 
+                    -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"
+                                />
+                                {/* <BlobImage
+                    xlink:href="assets/img/perfil.png"
+                    x="12"
+                    y="18"
+                  /> */}
+                            </g>
+                        </HomeBlob>
+                    </HomeImage>
+
+                    <HomeData>
+                        <HomeTitle>Hi, I'am Omar</HomeTitle>
+                        <HomeSubtitle>Frontend developer</HomeSubtitle>
+                        <HomeDescription>
+                            High level experience in web design and development knowledge,
+                            producing quality work.
+                        </HomeDescription>
+                        <Button href="#contact" className="button--flex">
+                            Contact Me <UilMessage className="button__icon" />
+                        </Button>
+                    </HomeData>
+                </HomeContent>
+
+                <HomeScroll>
+                    <ScrollButton href="#about" className="button--flex">
+                        <UilMouseAlt className="scroll__mouse" />
+                        <ScrollButtonText> Scroll down </ScrollButtonText>
+                        <UilArrowDown className="scroll__arrow" />
+                    </ScrollButton>
+                </HomeScroll>
+            </HomeContaier>
+        </Section>
     );
 };
 
--- src/Components/Home/styledHome.js
+++ src/Components/Home/styledHome.js
@@ -1,1 +1,83 @@
 import styled from "styled-components/macro";
+
+export const HomeContaier = styled.div`
+  gap: 1rem;
+`;
+
+export const HomeContent = styled.div`
+  grid-template-columns: 0.5fr 3fr;
+  padding-top: 3.5rem;
+  align-items: center;
+`;
+
+export const HomeSocial = styled.div`
+  display: grid;
+  grid-template-columns: max-content;
+  row-gap: 1rem;
+`;
+
+export const SocialIcon = styled.a`
+  font-size: 1.25rem;
+  color: ${({ theme }) => theme.colors.firstColor};
+
+  :hover {
+    color: ${({ theme }) => theme.colors.firstColorAlt};
+  }
+`;
+
+export const HomeImage = styled.div``;
+
+export const HomeBlob = styled.svg`
+  width: 200px;
+  fill: ${({ theme }) => theme.colors.firstColor};
+`;
+
+export const BlobImage = styled.img`
+  width: 170px;
+`;
+
+export const HomeData = styled.div`
+  grid-column: 1/3;
+`;
+
+export const HomeTitle = styled.h1`
+  font-size: ${({ theme }) => theme.typography.bigFontSize};
+`;
+
+export const HomeSubtitle = styled.h3`
+  font-size:  ${({ theme }) => theme.typography.h3FontSize};
+  color: var(--text-color);
+  margin-bottom:  ${({ theme }) => theme.marginBottom.mb_0_75};
+`;
+
+export const HomeDescription = styled.p`
+  margin-bottom:  ${({ theme }) => theme.marginBottom.mb_2};
+`
+
+export const HomeScroll = styled.div`
+  /* display: none; */
+`;
+
+export const ScrollButton = styled.a`
+  color: ${({ theme }) => theme.colors.firstColor};
+  transition: 0.3s;
+
+  :hover {
+    transform: translateY(0.25rem);
+  }
+
+  .scroll__mouse {
+    font-size: 2rem;
+  }
+
+  .scroll__arrow {
+    font-size: 1.25rem;
+  }
+`;
+
+export const ScrollButtonText = styled.span`
+  font-size: ${({ theme }) => theme.typography.smallFontSize};
+  color: ${({ theme }) => theme.colors.titleColor};
+  font-weight: ${({ theme }) => theme.typography.fontWeight.fontMedium};
+  margin-right: ${({ theme }) => theme.marginBottom.mb_0_25};
+`;
