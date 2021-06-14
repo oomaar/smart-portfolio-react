e430a6329194f0b7a3cb137ac042efb6c1c46120
Omar <omaar_5@hotmail.com>
2021-06-15 00:25:51 +0200

    feat: Skill Part-1

--- src/Components/Skill/Skill.js
+++ src/Components/Skill/Skill.js
@@ -1,12 +1,51 @@
+import UilBracketsCurly from "@iconscout/react-unicons/icons/uil-brackets-curly";
+import UilAngleDown from "@iconscout/react-unicons/icons/uil-angle-down";
 import {
-    SkillsContent,
+    SkillContent,
+    SkillHeader,
+    SkillTitle,
+    SkillSubTitle,
+    SkillList,
+    SkillData,
+    SkillTitles,
+    SkillName,
+    SkillNumber,
+    SkillBar,
+    SkillPercentage,
 } from "./styledSkill";
+import skillsData from "../../data/skillsData.json";
 
 const Skill = () => {
     return (
-        <SkillsContent>
-            Single Skill
-        </SkillsContent >
+        <div>
+            {skillsData.map(skill => (
+                <SkillContent key={skill.id}>
+                    <SkillHeader>
+                        <UilBracketsCurly className=" skills__icon" />
+
+                        <div>
+                            <SkillTitle>{skill.title}</SkillTitle>
+                            <SkillSubTitle>{skill.subTitle}</SkillSubTitle>
+                        </div>
+
+                        <UilAngleDown className="skills__arrow" />
+                    </SkillHeader>
+                    {skill.skills.map(item => (
+                        <SkillList className="grid" key={item.id}>
+                            <SkillData>
+                                <SkillTitles>
+                                    <SkillName>{item.name}</SkillName>
+                                    <SkillNumber>{item.number}</SkillNumber>
+                                </SkillTitles>
+                                <SkillBar>
+                                    <SkillPercentage className="skills__html"></SkillPercentage>
+                                </SkillBar>
+                            </SkillData>
+                        </SkillList>
+                    ))}
+                </SkillContent>
+            ))}
+        </div>
     );
 };
 
--- src/Components/Skill/styledSkill.js
+++ src/Components/Skill/styledSkill.js
@@ -1,3 +1,24 @@
 import styled from "styled-components/macro";
 
-export const SkillsContent = styled.div``;
+export const SkillContent = styled.div``;
+
+export const SkillHeader = styled.div``;
+
+export const SkillTitle = styled.h1``;
+
+export const SkillSubTitle = styled.span``;
+
+export const SkillList = styled.div``;
+
+export const SkillData = styled.div``;
+
+export const SkillTitles = styled.div``;
+
+export const SkillName = styled.h3``;
+
+export const SkillNumber = styled.span``;
+
+export const SkillBar = styled.span``;
+
+export const SkillPercentage = styled.div``;
+
