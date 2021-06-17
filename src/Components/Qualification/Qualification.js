1759974942b63f00b2c87a3c68302009dde7306e
Omar <omaar_5@hotmail.com>
2021-06-17 16:01:16 +0200

    feat: Qualification part-1

--- src/Components/Qualification/Qualification.js
+++ src/Components/Qualification/Qualification.js
@@ -1,12 +1,192 @@
+import UilGraduationCap from "@iconscout/react-unicons/icons/uil-graduation-cap";
+import UilBriefcaseAlt from "@iconscout/react-unicons/icons/uil-briefcase-alt";
+import UilCalendarAlt from "@iconscout/react-unicons/icons/uil-calendar-alt";
+import { Section, SectionSubtitle, SectionTitle } from "../../GlobalStyle";
 import {
-    Test,
+    QualificationContainer,
+    QualificationTabs,
+    QualificationButton,
+    QualificationSections,
+    QualificationContent,
+    QualificationData,
+    QualificationTitle,
+    QualificationSubtitle,
+    QualificationCalendar,
+    QualificationRounder,
+    QualificationLine,
 } from "./styledQualification";
+import { useState } from "react";
 
 const Qualification = () => {
+    const [dataTarget, setDataTarget] = useState('education');
+
     return (
-        <Test>
-            <h1>Qualification</h1>
-        </Test>
+        <Section>
+            <SectionTitle>Qualification</SectionTitle>
+            <SectionSubtitle>My personal journey</SectionSubtitle>
+
+            <QualificationContainer className="container">
+                <QualificationTabs>
+                    <QualificationButton
+                        onClick={() => setDataTarget('education')}
+                        class="button--flex"
+                        data-target="#education"
+                    >
+                        <UilGraduationCap className="qualification__icon" />
+                        Education
+                    </QualificationButton>
+
+                    <QualificationButton
+                        onClick={() => setDataTarget('work')}
+                        class="button--flex"
+                        data-target="#work"
+                    >
+                        <UilBriefcaseAlt className="qualification__icon" />
+                        Work
+                    </QualificationButton>
+                </QualificationTabs>
+
+                <QualificationSections>
+                    {/* QUALIFICATION CONTENT 1 */}
+                    <QualificationContent
+                        className={`${dataTarget === 'education' ? 'qualification__active' : ''}`}
+                        data-content
+                        id="education"
+                    >
+                        {/* QUALIFICATION 1*/}
+                        <QualificationData>
+                            <div>
+                                <QualificationTitle>Computer Enginner</QualificationTitle>
+                                <QualificationSubtitle>Peru - University</QualificationSubtitle>
+                                <QualificationCalendar>
+                                    <UilCalendarAlt />
+                                    2009 - 2014
+                                </QualificationCalendar>
+                            </div>
+
+                            <div>
+                                <QualificationRounder></QualificationRounder>
+                                <QualificationLine></QualificationLine>
+                            </div>
+                        </QualificationData>
+
+                        {/* QUALIFICATION 2*/}
+                        <QualificationData>
+                            <div></div>
+
+                            <div>
+                                <QualificationRounder></QualificationRounder>
+                                <QualificationLine></QualificationLine>
+                            </div>
+
+                            <div>
+                                <QualificationTitle>Web Design</QualificationTitle>
+                                <QualificationSubtitle>Spain - Institute</QualificationSubtitle>
+                                <QualificationCalendar>
+                                    <UilCalendarAlt />
+                                    2014 - 2017
+                                </QualificationCalendar>
+                            </div>
+                        </QualificationData>
+
+                        {/* QUALIFICATION 3*/}
+                        <QualificationData>
+                            <div>
+                                <QualificationTitle>Web Development</QualificationTitle>
+                                <QualificationSubtitle>Peru - Institute</QualificationSubtitle>
+                                <QualificationCalendar>
+                                    <UilCalendarAlt />
+                                    2017 - 2019
+                                </QualificationCalendar>
+                            </div>
+
+                            <div>
+                                <QualificationRounder></QualificationRounder>
+                                <QualificationLine></QualificationLine>
+                            </div>
+                        </QualificationData>
+
+                        {/* QUALIFICATION 4*/}
+                        <QualificationData>
+                            <div></div>
+
+                            <div>
+                                <QualificationRounder></QualificationRounder>
+                            </div>
+
+                            <div>
+                                <QualificationTitle>Master in Ui/Ux</QualificationTitle>
+                                <QualificationSubtitle>Peru - Institute</QualificationSubtitle>
+                                <QualificationCalendar>
+                                    <UilCalendarAlt />
+                                    2019 - 2021
+                                </QualificationCalendar>
+                            </div>
+                        </QualificationData>
+                    </QualificationContent>
+
+                    {/* QUALIFICATION CONTENT 2 */}
+                    <QualificationContent
+                        className={`${dataTarget === 'work' ? 'qualification__active' : ''}`}
+                        data-content
+                        id="work"
+                    >
+                        {/* QUALIFICATION 1*/}
+                        <QualificationData>
+                            <div>
+                                <QualificationTitle>Software Enginner</QualificationTitle>
+                                <QualificationSubtitle>Microsoft - Peru</QualificationSubtitle>
+                                <QualificationCalendar>
+                                    <UilCalendarAlt />
+                                    2016 - 2018
+                                </QualificationCalendar>
+                            </div>
+
+                            <div>
+                                <QualificationRounder></QualificationRounder>
+                                <QualificationLine></QualificationLine>
+                            </div>
+                        </QualificationData>
+
+                        {/* QUALIFICATION 2*/}
+                        <QualificationData>
+                            <div></div>
+
+                            <div>
+                                <QualificationRounder></QualificationRounder>
+                                <QualificationLine></QualificationLine>
+                            </div>
+
+                            <div>
+                                <QualificationTitle>Frontend developer</QualificationTitle>
+                                <QualificationSubtitle>Apple Inc - Spain</QualificationSubtitle>
+                                <QualificationCalendar>
+                                    <UilCalendarAlt />
+                                    2018 - 2020
+                                </QualificationCalendar>
+                            </div>
+                        </QualificationData>
+
+                        {/* QUALIFICATION 3*/}
+                        <QualificationData>
+                            <div>
+                                <QualificationTitle>UI Designer</QualificationTitle>
+                                <QualificationSubtitle>Figma - Spain</QualificationSubtitle>
+                                <QualificationCalendar>
+                                    <UilCalendarAlt />
+                                    2020 - Present
+                                </QualificationCalendar>
+                            </div>
+
+                            <div>
+                                <QualificationRounder></QualificationRounder>
+                                <QualificationLine></QualificationLine>
+                            </div>
+                        </QualificationData>
+                    </QualificationContent>
+                </QualificationSections>
+            </QualificationContainer>
+        </Section>
     );
 };
 
--- src/Components/Qualification/styledQualification.js
+++ src/Components/Qualification/styledQualification.js
@@ -1,3 +1,76 @@
 import styled from "styled-components/macro";
 
-export const Test = styled.div``;
+export const QualificationContainer = styled.div``;
+
+export const QualificationTabs = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin-bottom: ${({ theme }) => theme.marginBottom.mb_2};
+`;
+
+export const QualificationButton = styled.div`
+  font-size: ${({ theme }) => theme.typography.h3FontSize};
+  font-weight: ${({ theme }) => theme.typography.fontWeight.fontMedium};
+  cursor: pointer;
+  display: flex;
+
+  :hover {
+    color: ${({ theme }) => theme.colors.firstColor};
+  }
+
+  .qualification__icon {
+    font-size: 1.8rem;
+    margin-right: ${({ theme }) => theme.marginBottom.mb_0_25};
+    margin-left: ${({ theme }) => theme.marginBottom.mb_0_25};
+  }
+`;
+
+export const QualificationSections = styled.div``;
+
+export const QualificationContent = styled.div`
+  &[data-content] {
+      display: none;
+  }
+
+  &.qualification__active[data-content] {
+      display: block;
+  }
+`;
+
+export const QualificationData = styled.div`
+  display: grid;
+  grid-template-columns: 1fr max-content 1fr;
+  column-gap: 1.5rem;
+`;
+
+export const QualificationTitle = styled.h3`
+  font-size: ${({ theme }) => theme.typography.normalFontSize};
+  margin-right: ${({ theme }) => theme.typography.fontWeight.fontMedium};
+`;
+
+export const QualificationSubtitle = styled.span`
+  display: inline-block;
+  font-size: ${({ theme }) => theme.typography.smallFontSize};
+  margin-bottom: ${({ theme }) => theme.marginBottom.mb_1};
+`;
+
+export const QualificationCalendar = styled.div`
+  font-size: ${({ theme }) => theme.typography.smallerFontSize};
+  color: ${({ theme }) => theme.colors.textColorLight};
+`;
+
+export const QualificationRounder = styled.span`
+  display: inline-block;
+  width: 13px;
+  height: 13px;
+  background-color: ${({ theme }) => theme.colors.firstColor};
+  border-radius: 50%;
+`;
+
+export const QualificationLine = styled.span`
+  display: block;
+  width: 1px;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.firstColor};
+  transform: translate(6px, -7px);
+`;
