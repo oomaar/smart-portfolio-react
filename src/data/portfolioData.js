e96353ec18d4984c9b272b5a8fe74f6096d2ec2e
Omar <omaar_5@hotmail.com>
2021-06-18 02:15:07 +0200

    feat: setup portfolioData

---
+++ src/data/portfolioData.js
@@ -0,0 +1,23 @@
+export const portfolioData = [
+    {
+        id: 1,
+        src: "",
+        alt: "Portfolio1",
+        title: "Portfolio1",
+        description: "Website adaptable to all devices, with ui components andanimated interactions."
+    },
+    {
+        id: 2,
+        src: "",
+        alt: "Portfolio2",
+        title: "Portfolio2",
+        description: "Website adaptable to all devices, with ui components andanimated interactions."
+    },
+    {
+        id: 3,
+        src: "",
+        alt: "Portfolio3",
+        title: "Portfolio3",
+        description: "Website adaptable to all devices, with ui components andanimated interactions."
+    }
+];
