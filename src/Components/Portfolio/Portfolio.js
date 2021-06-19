40befa242b1bf0b9cdb456f1edc525dc31106742
Omar <omaar_5@hotmail.com>
2021-06-19 20:57:55 +0200

    feat: Portfolio part-1

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,12 +1,85 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import 'swiper/swiper-bundle.css';
+import { portfolioData } from "../../data/portfolioData";
+import UilArrowRight from "@iconscout/react-unicons/icons/uil-arrow-right";
+import UilAngleRightB from "@iconscout/react-unicons/icons/uil-angle-right-b";
+import UilAngleleftB from "@iconscout/react-unicons/icons/uil-angle-left-b";
+import { Button, Section, SectionSubtitle, SectionTitle } from "../../GlobalStyle";
 import {
     PortfolioContainer,
+    PortfolioContent,
+    PortfolioImage,
+    PortfolioData,
+    PortfolioTitle,
+    PortfolioDesc,
+    ArrowNext,
+    ArrowPrev,
 } from "./styledPortfolio";
+import { useRef } from 'react';
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 const Portfolio = () => {
+    const navigationPrevRef = useRef(null);
+    const navigationNextRef = useRef(null);
+
     return (
-        <PortfolioContainer>
-            Portfolio
-        </PortfolioContainer>
+        <Section>
+            <SectionTitle>Portfolio</SectionTitle>
+            <SectionSubtitle>Most recent work</SectionSubtitle>
+
+            <PortfolioContainer className="container">
+                <Swiper
+                    loop='true'
+                    navigation={{
+                        nextEl: navigationNextRef.current,
+                        prevEl: navigationPrevRef.current
+                    }}
+                    onSwiper={(swiper) => {
+                        // Delay execution for the refs to be defined
+                        setTimeout(() => {
+                            // Override prevEl & nextEl now that refs are defined
+                            swiper.params.navigation.prevEl = navigationPrevRef.current
+                            swiper.params.navigation.nextEl = navigationNextRef.current
+
+                            // Re-init navigation
+                            swiper.navigation.destroy()
+                            swiper.navigation.init()
+                            swiper.navigation.update()
+                        })
+                    }}
+                    pagination={{ clickable: true }}
+                >
+                    <PortfolioContent>
+                        {portfolioData.map(slide => (
+                            <SwiperSlide className='grid' key={slide.id}>
+                                <PortfolioImage src={slide.src} alt={slide.alt} />
+                                <PortfolioData>
+                                    <PortfolioTitle>{slide.title}</PortfolioTitle>
+                                    <PortfolioDesc>{slide.description}</PortfolioDesc>
+                                    <Button
+                                        href="#"
+                                        className="button--flex button--small portfolio__button"
+                                    >
+                                        Demo
+                                        <UilArrowRight className="button__icon" />
+                                    </Button>
+                                </PortfolioData>
+                            </SwiperSlide>
+                        ))}
+                    </PortfolioContent>
+                </Swiper>
+
+                <ArrowNext ref={navigationNextRef} className="swiper-button-next">
+                    <UilAngleRightB className="swiper-portfolio-icon" />
+                </ArrowNext>
+                <ArrowPrev ref={navigationPrevRef} className="swiper-button-prev">
+                    <UilAngleleftB className="swiper-portfolio-icon" />
+                </ArrowPrev>
+                <div className="swiper-pagination"></div>
+            </PortfolioContainer>
+        </Section>
     );
 };
 
--- src/Components/Portfolio/styledPortfolio.js
+++ src/Components/Portfolio/styledPortfolio.js
@@ -1,3 +1,78 @@
 import styled from "styled-components/macro";
 
-export const PortfolioContainer = styled.div``;
+export const PortfolioContainer = styled.div`
+  overflow: initial;
+  position: relative;
+
+  .swiper-container-horizontal > .swiper-pagination-bullets {
+    bottom: 0;
+    /* bottom: -2.5rem; */
+  }
+
+  .swiper-pagination-bullet-active {
+    background-color: ${({ theme }) => theme.colors.firstColor};
+  }
+
+  .swiper-button-prev,
+  .swiper-button-next,
+  .swiper-pagination-bullet {
+    outline: none;
+  }
+`;
+
+export const PortfolioContent = styled.div`
+  padding: 0 1.5rem;
+`;
+
+export const PortfolioImage = styled.img`
+  width: 265px;
+  border-radius: 0.5rem;
+  justify-self: center;
+`;
+
+export const PortfolioData = styled.div`
+   margin: 30px 0;
+
+  .portfolio__button {
+    :hover .button__icon {
+           transform: translateX(0.25rem);
+        }
+    }
+`;
+
+export const PortfolioTitle = styled.h3`
+  font-size: ${({ theme }) => theme.typography.h3FontSize};
+  margin-bottom: ${({ theme }) => theme.marginBottom.mb_5};
+`;
+
+export const PortfolioDesc = styled.p`
+  margin-bottom: ${({ theme }) => theme.marginBottom.mb_0_75};
+`;
+
+export const ArrowNext = styled.div`
+  right: -0.5rem;
+
+  .swiper-portfolio-icon {
+    font-size: 2rem;
+    color: ${({ theme }) => theme.colors.firstColor};
+    cursor: pointer;
+  }
+
+    ::after {
+      content: '';
+    }
+`;
+
+export const ArrowPrev = styled.div`
+  left: -0.5rem;
+
+  .swiper-portfolio-icon {
+    font-size: 2rem;
+    color: ${({ theme }) => theme.colors.firstColor};
+    cursor: pointer;
+  }
+
+  ::after {
+    content: '';
+  }
+`;
