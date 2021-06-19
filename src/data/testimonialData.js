0bca4a96f1924b34a9d320212701c61663e2f9b1
Omar <omaar_5@hotmail.com>
2021-06-19 21:34:46 +0200

    feat: setup testimnialData

---
+++ src/data/testimonialData.js
@@ -0,0 +1,23 @@
+export const testimonialData = [
+    {
+        src: "/img/testimonial1.jpg",
+        alt: "testimonial-1",
+        name: "Sara Smith",
+        client: "Client",
+        description: "I get a good impression, I carry out my project with all the possible and attention and support 24 hours a day."
+    },
+    {
+        src: "/img/testimonial2.jpg",
+        alt: "testimonial-2",
+        name: "Matt Robinson",
+        client: "Client",
+        description: "I get a good impression, I carry out my project with all the possible and attention and support 24 hours a day."
+    },
+    {
+        src: "/img/testimonial3.jpg",
+        alt: "testimonial-3",
+        name: "Raul Harris",
+        client: "Client",
+        description: "I get a good impression, I carry out my project with all the possible and attention and support 24 hours a day."
+    }
+];
