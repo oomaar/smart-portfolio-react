508d83ebb1503e21d7418393d7f1c367083b1d81
Omar <omaar_5@hotmail.com>
2021-06-20 00:25:32 +0200

    feat: Contact Part-1

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,8 +1,90 @@
+import { Button, Section } from "../../GlobalStyle";
+import UilPhone from "@iconscout/react-unicons/icons/uil-phone";
+import UilEnvelope from "@iconscout/react-unicons/icons/uil-envelope";
+import UilMapMarker from "@iconscout/react-unicons/icons/uil-map-marker";
+import UilMessage from "@iconscout/react-unicons/icons/uil-message";
+import {
+    ContactContainer,
+    ContactInformarion,
+    ContactTitle,
+    ContactSubtitle,
+    ContactForm,
+    ContactInputs,
+    ContactInputBox,
+    ContactLabel,
+    ContactInput,
+    ContactTextArea,
+    SubmitForm,
+} from "./styledContact";
+
 const Contact = () => {
+
+    const sendMessage = e => {
+        e.preventDefault();
+    };
+
     return (
-        <div>
-            Contact
-        </div>
+        <Section>
+            <ContactContainer className="container grid">
+                <div>
+                    <ContactInformarion>
+                        <UilPhone className="contact__icon" />
+                        <div>
+                            <ContactTitle>Call ME</ContactTitle>
+                            <ContactSubtitle>002-0111-777-2222</ContactSubtitle>
+                        </div>
+                    </ContactInformarion>
+                    <ContactInformarion>
+                        <UilEnvelope className="contact__icon" />
+
+                        <div>
+                            <ContactTitle>Email</ContactTitle>
+                            <ContactSubtitle>email@email.com</ContactSubtitle>
+                        </div>
+                    </ContactInformarion>
+                    <ContactInformarion>
+                        <UilMapMarker className="contact__icon" />
+
+                        <div>
+                            <ContactTitle>Location</ContactTitle>
+                            <ContactSubtitle>
+                                Egypt - Cairo, Helipolis Ram #166
+                            </ContactSubtitle>
+                        </div>
+                    </ContactInformarion>
+                </div>
+
+                <ContactForm action="" onSubmit={sendMessage}>
+                    <ContactInputs className="grid">
+                        <ContactInputBox>
+                            <ContactLabel for="">Name</ContactLabel>
+                            <ContactInput type="text" />
+                        </ContactInputBox>
+                        <ContactInputBox>
+                            <ContactLabel for="">Email</ContactLabel>
+                            <ContactInput type="email" />
+                        </ContactInputBox>
+                        <ContactInputBox>
+                            <ContactLabel for="">Project</ContactLabel>
+                            <ContactInput type="text" />
+                        </ContactInputBox>
+                        <ContactInputBox>
+                            <ContactLabel for="">Message</ContactLabel>
+                            <ContactTextArea
+                                cols="0"
+                                rows="7"
+                            ></ContactTextArea>
+                        </ContactInputBox>
+                    </ContactInputs>
+                    <div>
+                        <SubmitForm type="submit" className="button--flex">
+                            Send Message
+                            <UilMessage className="button__icon" />
+                        </SubmitForm>
+                    </div>
+                </ContactForm>
+            </ContactContainer>
+        </Section >
     );
 };
 
--- src/Components/Contact/styledContact.js
+++ src/Components/Contact/styledContact.js
@@ -1,9 +1,79 @@
 import styled from "styled-components/macro";
 
-export const ContactContainer = styled.div``;
+export const ContactContainer = styled.div`
+  row-gap: 3rem;
+`;
 
-export const ContactInformarion = styled.div``;
+export const ContactInformarion = styled.div`
+  display: flex;
+  margin-bottom: ${({ theme }) => theme.marginBottom.mb_2};
 
-export const ContactTitle = styled.h3``;
+  .contact__icon {
+    font-size: 2rem;
+    color: ${({ theme }) => theme.colors.firstColor};
+    margin-right: ${({ theme }) => theme.marginBottom.mb_0_75};
+  }
+`;
 
-export const ContactSubtitle = styled.span``;
+export const ContactTitle = styled.h3`
+  font-size: ${({ theme }) => theme.typography.h3FontSize};
+  font-weight: ${({ theme }) => theme.typography.fontWeight.fontMedium};
+`;
+
+export const ContactSubtitle = styled.span`
+  font-size: ${({ theme }) => theme.typography.smallerFontSize};
+  color: ${({ theme }) => theme.colors.textColorLight};
+`;
+
+export const ContactForm = styled.form``;
+
+export const ContactInputs = styled.div``;
+
+export const ContactInputBox = styled.div`
+  background-color: ${({ theme }) => theme.colors.inputColor};
+  border-radius: 0.5rem;
+  padding: 0.75rem 1rem 0.25rem;
+`;
+
+export const ContactLabel = styled.label`
+  font-size: ${({ theme }) => theme.typography.smallerFontSize};
+  color: ${({ theme }) => theme.colors.titleColor};
+`;
+
+export const ContactInput = styled.input`
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.inputColor};
+  color: ${({ theme }) => theme.colors.textColor};
+  font-family: ${({ theme }) => theme.typography.bodyFont};
+  font-size: ${({ theme }) => theme.typography.normalFontSize};
+  border: 0;
+  outline: 0;
+  padding: 0.25rem 0.5rem 0.5rem 0;
+`;
+
+export const ContactTextArea = styled.textarea`
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.inputColor};
+  color: ${({ theme }) => theme.colors.textColor};
+  font-family: ${({ theme }) => theme.typography.bodyFont};
+  font-size: ${({ theme }) => theme.typography.normalFontSize};
+  border: 0;
+  outline: 0;
+  padding: 0.25rem 0.5rem 0.5rem 0;
+`;
+
+export const SubmitForm = styled.button`
+    background-color: ${({ theme }) => theme.colors.firstColor};
+    color: #eee;
+    padding: 1rem;
+    border-radius: 0.5rem;
+    font-weight: ${({ theme }) => theme.typography.fontWeight.fontMedium};
+    cursor: pointer;
+    border: 0;
+    outline: 0;
+    margin: 20px 0;
+
+    :hover {
+       background-color: ${({ theme }) => theme.colors.firstColorAlt};
+    }
+`;
