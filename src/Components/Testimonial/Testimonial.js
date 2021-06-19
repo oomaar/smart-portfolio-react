4a3e4f7d8985057e80c14b13f1c484d4ad5d644f
Omar <omaar_5@hotmail.com>
2021-06-19 21:48:14 +0200

    feat: Testimonial Part-1

--- src/Components/Testimonial/Testimonial.js
+++ src/Components/Testimonial/Testimonial.js
@@ -1,12 +1,60 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import 'swiper/swiper-bundle.css';
+import UilStar from "@iconscout/react-unicons/icons/uil-star";
+import { Section, SectionSubtitle, SectionTitle } from "../../GlobalStyle";
+import { testimonialData } from "../../data/testimonialData";
 import {
+    TestimonialContainer,
     TestimonialData,
+    TestimonialHeader,
+    TestimonialImage,
+    TestimonialName,
+    TestimonialClient,
+    TestimonialDesc,
 } from "./styledTestimonial";
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 const Testimonial = () => {
     return (
-        <TestimonialData>
-            Testimonial
-        </TestimonialData>
+        <Section>
+            <SectionTitle>Testimonial</SectionTitle>
+            <SectionSubtitle>My client saying</SectionSubtitle>
+
+            <TestimonialContainer className="container">
+                <Swiper
+                    loop='true'
+                    pagination={{ clickable: true }}
+                >
+                    {testimonialData.map(testimonial => (
+                        <SwiperSlide>
+                            <TestimonialData>
+                                <TestimonialHeader>
+                                    <TestimonialImage src={testimonial.src} alt={testimonial.alt} />
+
+                                    <div>
+                                        <TestimonialName>{testimonial.name}</TestimonialName>
+                                        <TestimonialClient>{testimonial.client}</TestimonialClient>
+                                    </div>
+                                </TestimonialHeader>
+
+                                <div>
+                                    <UilStar className="testimonial__icon-star" />
+                                    <UilStar className="testimonial__icon-star" />
+                                    <UilStar className="testimonial__icon-star" />
+                                    <UilStar className="testimonial__icon-star" />
+                                    <UilStar className="testimonial__icon-star" />
+                                </div>
+                            </TestimonialData>
+
+                            <TestimonialDesc>{testimonial.description}</TestimonialDesc>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+                <div className="swiper-pagination"></div>
+            </TestimonialContainer>
+        </Section>
     );
 };
 
--- src/Components/Testimonial/styledTestimonial.js
+++ src/Components/Testimonial/styledTestimonial.js
@@ -1,3 +1,43 @@
 import styled from "styled-components/macro";
 
-export const TestimonialData = styled.div``;
+export const TestimonialContainer = styled.div`
+  .testimonial__icon-star {
+    color: ${({ theme }) => theme.colors.firstColor};
+  }
+
+  .swiper-pagination-bullet-active {
+    background-color: ${({ theme }) => theme.colors.firstColor};
+  }
+`;
+
+export const TestimonialData = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: ${({ theme }) => theme.marginBottom.mb_1};
+`;
+
+export const TestimonialHeader = styled.div`
+  display: flex;
+`;
+
+export const TestimonialImage = styled.img`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  margin-right: ${({ theme }) => theme.marginBottom.mb_0_75};
+`;
+
+export const TestimonialName = styled.h3`
+  font-size: ${({ theme }) => theme.typography.h3FontSize};
+  font-weight: ${({ theme }) => theme.typography.fontWeight.fontMedium};
+`;
+
+export const TestimonialClient = styled.span`
+  font-size: ${({ theme }) => theme.typography.smallerFontSize};
+  color: ${({ theme }) => theme.colors.textColorLight};
+`;
+
+export const TestimonialDesc = styled.p`
+  margin-bottom: ${({ theme }) => theme.marginBottom.mb_2_5};
+
+`;
