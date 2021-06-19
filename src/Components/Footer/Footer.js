74b2570574ea36ca8cfb93e174f3ca617da32300
Omar <omaar_5@hotmail.com>
2021-06-20 00:49:12 +0200

    feat: Footer Part-1

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,55 @@
+import UilFacebookF from "@iconscout/react-unicons/icons/uil-facebook-f";
+import UilInstagram from "@iconscout/react-unicons/icons/uil-instagram";
+import UilTwitterAlt from "@iconscout/react-unicons/icons/uil-twitter-alt";
 import {
     FooterContainer,
+    FooterBackground,
+    FooterSubContainer,
+    FooterTitle,
+    FooterSubTitle,
+    FooterList,
+    FooterLink,
+    FooterCopy,
+    FooterSocials,
+    FooterSocial,
 } from "./styledFooter";
 
 const Footer = () => {
     return (
         <FooterContainer>
-            Footer
+            <FooterBackground>
+                <FooterSubContainer className="container grid">
+                    <div>
+                        <FooterTitle>Omar</FooterTitle>
+                        <FooterSubTitle>Frontend Developer</FooterSubTitle>
+                    </div>
+
+                    <FooterList>
+                        <li>
+                            <FooterLink href="#services">Services</FooterLink>
+                        </li>
+                        <li>
+                            <FooterLink href="#portfolio">Portfolio</FooterLink>
+                        </li>
+                        <li>
+                            <FooterLink href="#contact">Contact</FooterLink>
+                        </li>
+                    </FooterList>
+
+                    <FooterSocials>
+                        <FooterSocial href="#">
+                            <UilFacebookF />
+                        </FooterSocial>
+                        <FooterSocial href="#">
+                            <UilInstagram />
+                        </FooterSocial>
+                        <FooterSocial href="#">
+                            <UilTwitterAlt />
+                        </FooterSocial>
+                    </FooterSocials>
+                </FooterSubContainer>
+                <FooterCopy>&#169; All rights reserved</FooterCopy>
+            </FooterBackground>
         </FooterContainer>
     );
 };
--- src/Components/Footer/styledFooter.js
+++ src/Components/Footer/styledFooter.js
@@ -1,3 +1,59 @@
 import styled from "styled-components/macro";
 
-export const FooterContainer = styled.div``;
+export const FooterContainer = styled.footer`
+  padding-top: 2rem;
+`;
+
+export const FooterBackground = styled.div`
+  background-color: ${({ theme }) => theme.colors.firstColor};
+  padding: 2rem 0 3rem;
+`;
+
+export const FooterSubContainer = styled.div`
+  row-gap: 3.5rem;
+`;
+
+export const FooterTitle = styled.h1`
+  color: #fff;
+  font-size: ${({ theme }) => theme.typography.h1FontSize};
+  margin-bottom: ${({ theme }) => theme.marginBottom.mb_0_25};
+`;
+
+export const FooterSubTitle = styled.span`
+  color: #fff;
+  font-size: ${({ theme }) => theme.typography.smallFontSize};
+`;
+
+export const FooterList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  row-gap: 1.5rem;
+`;
+
+export const FooterLink = styled.a`
+  color: #fff;
+
+  :hover {
+      color: ${({ theme }) => theme.colors.firstColorLighter};
+  }
+`;
+
+export const FooterSocials = styled.div``;
+
+export const FooterSocial = styled.a`
+  font-size: 1.25rem;
+  margin-right: ${({ theme }) => theme.marginBottom.mb_1_5};
+  color: #fff;
+
+  :hover {
+    color: ${({ theme }) => theme.colors.firstColorLighter};
+  }
+`;
+
+export const FooterCopy = styled.p`
+  color: #fff;
+  font-size: ${({ theme }) => theme.typography.smallerFontSize};
+  text-align: center;
+  color: ${({ theme }) => theme.colors.textColorLight};
+  margin-top: ${({ theme }) => theme.marginBottom.mb_3};
+`;
