007fc444075d0284855e44285d11926cf54f9484
Omar <omaar_5@hotmail.com>
2021-06-22 21:26:47 +0200

    complete Contact

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,8 +1,9 @@
-import { Button, Section } from "../../GlobalStyle";
+import { Section, SectionSubtitle, SectionTitle } from "../../GlobalStyle";
 import UilPhone from "@iconscout/react-unicons/icons/uil-phone";
 import UilEnvelope from "@iconscout/react-unicons/icons/uil-envelope";
 import UilMapMarker from "@iconscout/react-unicons/icons/uil-map-marker";
 import UilMessage from "@iconscout/react-unicons/icons/uil-message";
+import { setHeight } from "../../utils/setTextareaHeight";
 import {
     ContactContainer,
     ContactInformarion,
@@ -21,10 +22,13 @@ const Contact = () => {
 
     const sendMessage = e => {
         e.preventDefault();
+        // Firebase code goes here
     };
 
     return (
         <Section id="contact">
+            <SectionTitle>Contact Me</SectionTitle>
+            <SectionSubtitle>Get in touch</SectionSubtitle>
             <ContactContainer className="container grid">
                 <div>
                     <ContactInformarion>
@@ -54,28 +58,29 @@ const Contact = () => {
                     </ContactInformarion>
                 </div>
 
-                <ContactForm action="" onSubmit={sendMessage}>
+                <ContactForm className="grid" action="" onSubmit={sendMessage}>
                     <ContactInputs className="grid">
                         <ContactInputBox>
-                            <ContactLabel for="">Name</ContactLabel>
+                            <ContactLabel>Name</ContactLabel>
                             <ContactInput type="text" />
                         </ContactInputBox>
                         <ContactInputBox>
-                            <ContactLabel for="">Email</ContactLabel>
+                            <ContactLabel>Email</ContactLabel>
                             <ContactInput type="email" />
                         </ContactInputBox>
-                        <ContactInputBox>
-                            <ContactLabel for="">Project</ContactLabel>
-                            <ContactInput type="text" />
-                        </ContactInputBox>
-                        <ContactInputBox>
-                            <ContactLabel for="">Message</ContactLabel>
-                            <ContactTextArea
-                                cols="0"
-                                rows="7"
-                            ></ContactTextArea>
-                        </ContactInputBox>
                     </ContactInputs>
+                    <ContactInputBox>
+                        <ContactLabel>Project</ContactLabel>
+                        <ContactInput type="text" />
+                    </ContactInputBox>
+                    <ContactInputBox>
+                        <ContactLabel>Message</ContactLabel>
+                        <ContactTextArea
+                            cols="0"
+                            rows="7"
+                            onChange={e => setHeight(e, '100px')}
+                        ></ContactTextArea>
+                    </ContactInputBox>
                     <div>
                         <SubmitForm type="submit" className="button--flex">
                             Send Message
@@ -84,7 +89,7 @@ const Contact = () => {
                     </div>
                 </ContactForm>
             </ContactContainer>
-        </Section >
+        </Section>
     );
 };
 
