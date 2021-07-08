732a4dfc012c64105834a843352cdfbee35a9935
Omar <omaar_5@hotmail.com>
2021-07-08 18:14:06 +0200

    refactor: Project

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,5 +1,5 @@
 import UilMessage from "@iconscout/react-unicons/icons/uil-message";
-import { Button, Section } from "../../GlobalStyle";
+import { Button, ButtonIcon, Container, Section } from "../../GlobalStyle";
 import {
     ProjectBackground,
     ProjectContainer,
@@ -13,21 +13,27 @@ const Project = () => {
     return (
         <Section>
             <ProjectBackground>
-                <ProjectContainer className="container grid">
-                    <ProjectData>
-                        <ProjectTitle>You have a new project !</ProjectTitle>
-                        <ProjectSubtitle>
-                            Contact me now and get a 30% discount on your new project.
-                        </ProjectSubtitle>
-                        <Button
-                            href="#contact"
-                            className="button--flex button--white">
-                            Contact Me
-                            <UilMessage className="project__icon button__icon" />
-                        </Button>
-                    </ProjectData>
-                    <ProjectImage src="/img/project.png" alt="Project img" />
-                </ProjectContainer>
+                <Container>
+                    <ProjectContainer className="grid">
+                        <ProjectData>
+                            <ProjectTitle>You have a new project !</ProjectTitle>
+                            <ProjectSubtitle>
+                                Contact me now and get a 30% discount on your new project.
+                            </ProjectSubtitle>
+                            <Button
+                                href="#contact"
+                                className="button--white"
+                                flex
+                            >
+                                Contact Me
+                                <ButtonIcon>
+                                    <UilMessage />
+                                </ButtonIcon>
+                            </Button>
+                        </ProjectData>
+                        <ProjectImage src="/img/project.png" alt="Project img" />
+                    </ProjectContainer>
+                </Container>
             </ProjectBackground>
         </Section>
     );
