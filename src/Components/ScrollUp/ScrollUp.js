fb26078073ef0fc9294b2b1ae355f8037506a031
Omar <omaar_5@hotmail.com>
2021-07-10 00:33:30 +0200

    complete: ScrollUp

--- src/Components/ScrollUp/ScrollUp.js
+++ src/Components/ScrollUp/ScrollUp.js
@@ -1,8 +1,16 @@
-const ScrollUp = () => {
+import UilArrowUp from "@iconscout/react-unicons/icons/uil-arrow-up";
+import {
+    ScrollUpLink,
+    ScrollupIcon,
+} from "./styledScrollUp";
+
+const ScrollUp = ({ show }) => {
     return (
-        <div>
-            Scroll UP
-        </div>
+        <ScrollUpLink href="#" show={show}>
+            <ScrollupIcon>
+                <UilArrowUp />
+            </ScrollupIcon>
+        </ScrollUpLink>
     );
 };
 
--- src/Components/ScrollUp/styledScrollUp.js
+++ src/Components/ScrollUp/styledScrollUp.js
@@ -1,1 +1,29 @@
 import styled from "styled-components/macro";
+
+export const ScrollUpLink = styled.a`
+  position: fixed;
+  right: 1rem;
+  bottom: ${({ show }) => show ? '5rem' : '-20%'};
+  background-color: ${({ theme }) => theme.colors.firstColor};
+  z-index: ${({ theme }) => theme.zIndex.zFixed};
+  opacity: 0.8;
+  padding: 0 0.3rem;
+  border-radius: 0.4rem;
+  transition: 0.4s;
+  justify-content: center;
+  padding: 0.3rem;
+
+  :hover {
+    background-color: ${({ theme }) => theme.colors.firstColorAlt};
+  }
+
+  @media screen and (min-width: 1200px) {
+    right: 10rem;
+  }
+`;
+
+export const ScrollupIcon = styled.div`
+  display: flex;
+  font-size: 1.5rem;
+  color: #fff;
+`;
